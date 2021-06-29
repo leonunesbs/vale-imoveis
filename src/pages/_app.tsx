454e330e2117a2e0814267073ff29b4c3ce96511
Leonardo Nunes <leonunesbs@gmail.com>
2021-06-29 17:38:49 -0300

    fix: gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import theme from '@/styles/theme';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-NR33KTV' });
+  }, []);
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
