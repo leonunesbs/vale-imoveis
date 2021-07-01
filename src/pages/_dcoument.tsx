18f85393fcd93d3293727fb147b89ab060890cdb
Leonardo Nunes <leonunesbs@gmail.com>
2021-07-01 11:35:10 -0300

    fix: fix

--- src/pages/_dcoument.tsx
+++ src/pages/_dcoument.tsx
@@ -16,12 +16,33 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-NR33KTV');`,
             }}
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `!function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '4281058651955284');
+              fbq('track', 'PageView');`,
+            }}
+          />
         </Head>
         <body>
           <noscript
             dangerouslySetInnerHTML={{
               __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NR33KTV"
 height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<img height="1" width="1" style="display:none"
+              src="https://www.facebook.com/tr?id=4281058651955284&ev=PageView&noscript=1"
+              />`,
             }}
           />
           <Main />
